13dff00b79d25df9ee1d7f804e901464664e3c80
lavanyaYarlagadda <lavanyaYarlagadda>
2025-09-01 17:51:13 +0530

    Added the app.tsx

---
+++ wine/src/App.tsx
@@ -0,0 +1,19 @@
+import FilterComponent from "./molecules/ProductList/Filter";
+import Home from "./pages/Home/Home";
+import ProductListPage from "./pages/ProductList/ProductListPage";
+
+
+
+
+const App = () => {
+
+
+
+  return (
+    <>
+      <Home/>
+    </>
+  );
+};
+
+export default App;
