71127e7f45a6746caf36f04616baf0c9d110c305
Harish.K <kollatiharish@gmail.com>
2025-09-06 09:41:47 +0530

    NO TICKET:eslint configs

---
+++ wine/config/.eslintrc.js
@@ -0,0 +1,69 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+		es2022: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2022,
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	ignorePatterns: [
+		'node_modules',
+		'build',
+		'dist',
+		'coverage',
+		'public',
+		'core',
+		'**/*.js',
+		'**/*.jsx',
+	],
+	rules: {
+		// Keep it minimal; rely on TS + React recommended rules
+		'no-console': 'warn',
+		'no-debugger': 'error',
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{ argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+		],
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/consistent-type-imports': [
+			'error',
+			{ prefer: 'type-imports' },
+		],
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-key': 'error',
+	},
+	overrides: [
+		{
+			files: ['**/*.{ts,tsx}'],
+			rules: {
+				'@typescript-eslint/no-non-null-assertion': 'off',
+			},
+		},
+		{
+			files: ['**/*.{test,spec}.{ts,tsx}'],
+			rules: {
+				'@typescript-eslint/no-explicit-any': 'off',
+				'@typescript-eslint/no-unused-vars': 'off',
+			},
+		},
+	],
+};
